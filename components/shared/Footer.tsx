6c2c8464e666fdfa3141bf53807a564da8968aab
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-03-19 23:13:05 +0100

    Create db schema

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,25 @@
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default Footer
+const Footer = () => {
+  const currentYear =  new Date().getFullYear()
+
+  return (
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href='/'>
+          <Image
+            src='/assets/images/logo.svg'
+            alt='logo'
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>{currentYear} Evently. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
